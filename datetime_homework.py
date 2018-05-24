e5871eec5d9a494671619f7091c26e9be399d029
funaska <dlya.glupostey@gmail.com>
2018-05-24 23:03:51 +0300

    finish datetime task

--- datetime_homework.py
+++ datetime_homework.py
@@ -7,3 +7,9 @@ print('Вчера было', str(yesterday.day) + '.' + str(yesterday.month) + '
 print('А сегодня', str(datetime.today().day) + '.' + str(datetime.today().month) + '.' + str(datetime.today().year) )
 
 print('Ну а месяц назад', str(datetime.today().day) + '.' + str(datetime.today().month - 1 ) + '.' + str(datetime.today().year) )
+
+task_string = '01/01/17 12:10:03.234567'
+
+target_datetime = datetime.strptime(task_string,'%m/%d/%y %H:%M:%S.%f')
+
+print(type(target_datetime))
